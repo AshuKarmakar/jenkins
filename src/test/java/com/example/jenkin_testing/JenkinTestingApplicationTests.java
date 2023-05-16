43e86e0d6e06b376111db7f30963bc73f0ac1f3a
ashutosh karmakar <ashutosh.karmakar@sap.com>
2023-05-16 12:47:00 +0530

    first commit

---
+++ src/test/java/com/example/jenkin_testing/JenkinTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkin_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
